de033dafa806bc14e15b127a123711400077634d
Steven Kitzes <steven.kitzes@gmail.com>
2021-02-23 01:02:09 -0800

    test: hide play in setTimeout to check iOS compatibility

--- test.js
+++ test.js
@@ -2,7 +2,7 @@ var audio = document.createElement('audio')
 audio.src = 'bojack.mp3'
 
 var button = document.createElement('button')
-button.innerText = 'Play bojack!'
+button.innerText = 'Play bojack with delay!'
 
 button.addEventListener('click', function () {
   if (audio.paused) {
